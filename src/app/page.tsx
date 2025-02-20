83e7ba97f2eb70e2410470479c74f155e72c39c6
rafinha-as-br <rafinha72465@gmail.com>
2025-02-19 22:33:58 -0300

    Final quarta feira

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { ArrowRight, Copy } from "lucide-react";
 import { Button } from "@/components/button";
 import { IconButton } from "@/components/iconButton";
+import { Input } from "@/components/input";
 
 
 export default function Home() {
   return (
     <main>
-      <Button> 
+      <Button type="submit"> 
         enviar
         <ArrowRight /> 
       </Button>
@@ -14,6 +15,9 @@ export default function Home() {
       <IconButton>
         <Copy/>
       </IconButton>
+
+      <Input type="email" placeholder="Digite seu e-mail" ></Input>
+      <Input type="email" placeholder="Digite seu e-mail" error ></Input>
     </main>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,8 +1,6 @@
 import { ComponentProps, ReactNode } from "react"
 
-interface ButtonProps extends ComponentProps<'button'>{
-    children: ReactNode;
-}
+interface ButtonProps extends ComponentProps<'button'>{}
 
 export function Button(props: ButtonProps){
     return <button className="flex justify-between items-center px-5 h-12 bg-gray-500 font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-500 " {...props}>
